1d7f91ac677121088e23957fa12fb9a6110b7705
skatlamenace <66825424+skatlamenace@users.noreply.github.com>
2020-11-02 17:34:31 +0100

    Update BatchCalculatorTest.java

--- src/test/java/calculator/BatchCalculatorTest.java
+++ src/test/java/calculator/BatchCalculatorTest.java
@@ -26,7 +26,7 @@ class BatchCalculatorTest {
 		
 		// Aller chercher la donnée fournie par un collaborateur sur Internet
 		try (BufferedReader br = new BufferedReader(new InputStreamReader(new URL(
-				"https://gist.githubusercontent.com/geoffreyarthaud/7855a0f4322421f164f1701694f4369d/raw/5ad5f559f010c45c3aa9f8d266618ed5d314d5ea/operations.txt")
+				"https://raw.githubusercontent.com/skatlamenace/TD3-Calculator/main/src/test/resources/dataset/operations.txt")
 						.openStream()))) {
 			operations = br.lines();
 
